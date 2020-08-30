cf3bdce9b8b439d5582850f098bdcc726c645e15
Rocio Vargas <vargas.rocio@hotmail.com.ar>
2020-08-30 14:25:01 -0300

    Resolución Ejercicio clase 18-08

--- script.js
+++ script.js
@@ -10,6 +10,40 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+let tareasPendientes=0;
+let totalTareas=0;
+
+
 function addTodo() {
-  alert('Boton Add TODO clickeado!')
+  let lista = document.createElement("li");
+  lista.classname=classNames.TODO_ITEM;
+  
+  let checkbox= document.createElement("input");
+  checkbox.classname = classNames.TODO_CHECKBOX;
+  checkbox.type = "checkbox";
+
+  totalTareas ++
+  itemCountSpan.innerText=totalTareas.toString();
+  tareasPendientes++;
+  uncheckedCountSpan.innerText=tareasPendientes.toString();
+
+  checkbox.onchange=function(){
+    if(checkbox.click){
+    tareasPendientes--;
+    }else{
+    tareasPendientes++;
+    }
+    uncheckedCountSpan.innerText=tareasPendientes.toString();
+  }
+
+  let texto = document.createElement("span");
+  texto.classname=classNames.TODO_TEXT;
+  texto.setAttribute("editable", true);
+  texto.innerHTML = `Tarea Nro. ${totalTareas}`;  
+
+  lista.appendChild(checkbox);
+  lista.appendChild(texto);
+  list.appendChild(lista);
+  
+  
 }
